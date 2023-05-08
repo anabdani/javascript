376c45206b56c0d97f7f847ac70a463902dcbb23
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2023-05-08 13:39:20 -0300

    exercicios

---
+++ aula12/ex013.js
@@ -0,0 +1,33 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+console.log(diaSem)
+
+switch(diaSem) {
+    case 0 :
+       console.log('Domindo')
+       break 
+     case 1 :
+        console.log('segunda')
+        break
+      case 2 :
+            console>log('Terça')
+            break
+     case 3 :
+             console>log('Quarta')
+             break
+     case 4 :
+             console>log('Quinta')
+             break
+     case 5 :
+             console>log('Sexta')
+             break
+     case 6:
+          console>log('Sábado')
+          break 
+          
+          default:
+            console.log('[ERRO] dia inválido')
+            break
+
+}
