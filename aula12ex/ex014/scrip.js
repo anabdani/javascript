1e31d55cd73ba96c5e748b02b631cbe182804899
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2023-05-17 15:33:37 -0300

    site

--- aula12ex/ex014/scrip.js
+++ aula12ex/ex014/scrip.js
@@ -1,8 +1,20 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `agora são ${hora} horas.`
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
 
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.jpg'
+        document.body.style.background = '#e2cd9f'
+
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#ef8d53'
+
+    } else {
+        img.src = 'noite.jpg'
+        document.body.style.background =  '#515154'
+    }
 }
