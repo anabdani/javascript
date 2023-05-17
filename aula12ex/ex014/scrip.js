371ffe4e6981afe35f37d3e25eb85ca18bb18173
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2023-05-17 14:14:44 -0300

    atualização

--- aula12ex/ex014/scrip.js
+++ aula12ex/ex014/scrip.js
@@ -1,1 +1,8 @@
-alert("olá")
+function carregar() {
+    var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `agora são ${hora} horas.`
+
+}
