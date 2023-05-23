a90739d5e5d8b3e7b23d81e9eb3ea92ca2f00f5e
ana beatriz dani bernardo <anadani2526@gmail.com>
2023-05-23 13:35:24 -0300

    estrutura de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+/*
+var c = 1
+while (c <= 500) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('tudo bem?')
+*/
