97696f5698acb5e739c414af7f907ac9590b95ac
ana beatriz dani bernardo <anadani2526@gmail.com>
2023-06-05 11:09:49 -0300

    função

---
+++ aula16/função01.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par!'
+    } else {
+        return  'ímpar!'
+    }
+}
+
+let res = parimpar(4)
+console.log(res)
+/* ou podemos usar: console.log(parimpar(56)) */
